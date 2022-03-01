961c0dbdd772fa741f2b52b04f342ea9410f9a5e
Sheeepycheap <alinejadkevin@gmail.com>
2022-03-01 10:08:48 +0100

    first commit test

---
+++ test.py
@@ -0,0 +1,29 @@
+import MetaTrader5 as mt5 
+
+mt5.initialize()
+print("ok")
+login = 41552251
+password = "ee4rli8A1iLU"
+server = "AdmiralMarkets-Demo"
+mt5.login(login,password,server)
+
+print("ok")
+
+account_info = mt5.account_info()
+
+request = {
+    "action" : mt5.TRADE_ACTION_DEAL  ,  
+    "symbol" : "BTCUSD" ,
+    "volume" : 4.0 ,
+    "type" : mt5.ORDER_TYPE_BUY ,
+    "price" : mt5.symbol_info_tick("BTCUSD").ask,
+    "sl" : 0.0 ,
+    "tp" : 0.0 ,
+    "deviation" : 20 ,
+    "magic" : 234000 ,
+    "comment" : "test",
+    "type_time" : mt5.ORDER_TIME_GTC,
+    "type filling" : mt5.ORDER_FILLING_IOC,
+}
+order = mt5.order_send(request)
+print(order)
